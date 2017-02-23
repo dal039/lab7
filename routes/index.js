2ef80d09d0dbf717912b9c670745003e5f322572
David Lee <dal039@ucsd.edu>
2017-02-23 11:30:22 -0800

    saf

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	var min;
+	projects["min"] = false;
   	res.render('index', projects);
 };
+
+exports.viewMin = function(req, res) {
+	var min;
+	projects["min"] = true;
+	res.render('index', projects);
+}
